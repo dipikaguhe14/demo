55b07e48c93d134764383aa26fcba781a6f2382b
Dipika Guhe <38162480+dipikaguhe14@users.noreply.github.com>
2020-01-28 14:03:00 +0530

    video1

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dipika-demo-deployment'));
+app.use(express.static(__dirname + '/demo'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dipika-demo-deployment/index.html'));
+res.sendFile(path.join(__dirname+'/demo/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
